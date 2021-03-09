233b07cc775984c06447e49c6de7c27f1ee085c1
Alonzo Manaloto <alonzo@elephantventures.com>
2021-03-09 18:11:50 +0400

    added model for Employee

---
+++ etx-back-end/src/app/model/employee.model.js
@@ -0,0 +1,9 @@
+function Employee(id,
+                  firstName,
+                  lastName){
+                      this.id = id
+                      this.firstName = firstName
+                      this.lastName = lastName
+                  }
+
+module.exports = Employee
